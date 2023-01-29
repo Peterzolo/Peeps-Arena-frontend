e62164f1f48c2110d4a1c429fd76852251a8846c
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-01-29 20:33:32 +0100

    Input components
    Added input components

---
+++ src/components/input/Input.jsx
@@ -0,0 +1,24 @@
+const Input = forwardRef((props, ref) => (
+  <div className="form-row">
+    {props.labelText && (
+      <label htmlFor={props.name} className="form-label">
+        {props.labelText}
+      </label>
+    )}
+    <input
+      ref={ref}
+      id={props.id}
+      name={props.name}
+      type={props.type}
+      value={props.value}
+      onChange={props.handleChange}
+      placeholder={props.placeholder}
+      onClick={props.onClick}
+      onFocus={props.onFocus}
+      onBlur={props.onBlur}
+      className={`form-input ${props.className}`}
+      style={props.style}
+      autoComplete="false"
+    />
+  </div>
+));
