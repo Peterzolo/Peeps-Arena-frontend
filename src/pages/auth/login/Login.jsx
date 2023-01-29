ec72be304b51e52c32a2c2adb700d84dbe413d33
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-01-29 21:11:54 +0100

    Update Login.jsx
    Added input

--- src/pages/auth/login/Login.jsx
+++ src/pages/auth/login/Login.jsx
@@ -2,7 +2,60 @@ import './Login.scss';
 import React from 'react';
 
 const Login = () => {
-  return <div></div>;
+  return (
+    <div className="auth-inner">
+      {hasError && errorMessage && (
+        <div className={`alerts ${alertType}`} role="alert">
+          {errorMessage}
+        </div>
+      )}
+      <form className="auth-form" onSubmit={loginUser}>
+        <div className="form-input-container">
+          <Input
+            id="username"
+            name="username"
+            type="text"
+            value={username}
+            labelText="Username"
+            placeholder="Enter Username"
+            style={{ border: `${hasError ? '1px solid #fa9b8a' : ''}` }}
+            handleChange={(event) => setUsername(event.target.value)}
+          />
+          <Input
+            id="password"
+            name="password"
+            type="password"
+            value={password}
+            labelText="Password"
+            placeholder="Enter Password"
+            style={{ border: `${hasError ? '1px solid #fa9b8a' : ''}` }}
+            handleChange={(event) => setPassword(event.target.value)}
+          />
+          <label className="checkmark-container" htmlFor="checkbox">
+            <Input
+              id="checkbox"
+              name="checkbox"
+              type="checkbox"
+              value={keepLoggedIn}
+              handleChange={() => setKeepLoggedIn(!keepLoggedIn)}
+            />
+            Keep me signed in
+          </label>
+        </div>
+        <Button
+          label={`${loading ? 'SIGNIN IN PROGRESS...' : 'SIGNIN'}`}
+          className="auth-button button"
+          disabled={!username || !password}
+        />
+
+        <Link to={'/forgot-password'}>
+          <span className="forgot-password">
+            Forgot password? <FaArrowRight className="arrow-right" />
+          </span>
+        </Link>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
