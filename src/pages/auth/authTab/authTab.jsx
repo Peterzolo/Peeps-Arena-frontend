ea84507d1a28fb221fe0a2dbedfaa40b9a2fe965
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-02-02 18:56:45 +0100

    Update authTab.jsx
    modified

--- src/pages/auth/authTab/authTab.jsx
+++ src/pages/auth/authTab/authTab.jsx
@@ -1,46 +1,7 @@
-import React, { useState } from 'react';
-import { Row, Col } from 'react-bootstrap';
-// import backgroundImage from '../../../assets/images/background1.jpg';
-import Login from '../login/Login';
-import './authTab.scss';
+import React from 'react';
 
 const authTab = () => {
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const [type, setType] = useState('Sign In');
-  return (
-    <Row>
-      <Col md={12}>
-        {/* {keepLoggedIn ? (
-        <PageLoader />
-      ) : ( */}
-        {/* <div className="container-wrapper" style={{ backgroundImage: `url(${backgroundImage})` }}> */}
-        <div className="container-wrapper">
-          <div className="environment"></div>
-          <div className="container-wrapper-auth">
-            <div className="tabs">
-              <div className="tabs-auth">
-                <ul className="tab-group">
-                  <li className={`tab ${type === 'Sign In' ? 'active' : ''}`} onClick={() => setType('Sign In')}>
-                    <button className="login">Sign In</button>
-                  </li>
-                  <li className={`tab ${type === 'Sign Up' ? 'active' : ''}`} onClick={() => setType('Sign Up')}>
-                    <button className="signup">Sign Up</button>
-                  </li>
-                </ul>
-                {type === 'Sign In' && (
-                  <div className="tab-item">
-                    <Login />
-                  </div>
-                )}
-                {type === 'Sign Up' && <div className="tab-item">{/* <Register /> */}</div>}
-              </div>
-            </div>
-          </div>
-        </div>
-        {/* )} */}
-      </Col>
-    </Row>
-  );
+  return <div></div>;
 };
 
 export default authTab;
