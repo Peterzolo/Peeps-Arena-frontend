86cd1cdeaacd0953f1322f4fa161ca81c33deb12
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-02-04 00:34:52 +0100

    styles
    styles

--- src/pages/auth/auth-tab/AuthTam.jsx
+++ src/pages/auth/auth-tab/AuthTam.jsx
@@ -16,10 +16,10 @@ const AuthTam = () => {
                 <div className="tabs-auth">
                   <ul className="tab-group">
                     <li className={`tab ${type === 'Sign In' ? 'active' : ''}`} onClick={() => setType('Sign In')}>
-                      <button className="login">Sign In</button>
+                      <button className="login-btn">Sign In</button>
                     </li>
                     <li className={`tab ${type === 'Sign Up' ? 'active' : ''}`} onClick={() => setType('Sign Up')}>
-                      <button className="signup">Sign Up</button>
+                      <button className="register-btn">Sign Up</button>
                     </li>
                   </ul>
                   {type === 'Sign In' && <div className="tab-item">LOGIN</div>}
