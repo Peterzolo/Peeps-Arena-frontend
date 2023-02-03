b50727d67572975a62c6156ea58ca3d19c67f110
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-02-03 19:24:50 +0100

    Update AuthTam.jsx
    Modified

--- src/pages/auth/auth-tab/AuthTam.jsx
+++ src/pages/auth/auth-tab/AuthTam.jsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 
 const AuthTam = () => {
-  return <div>Auth Tab</div>;
+  const [type, setType] = useState('Sign In');
+  return (
+    <Container>
+      <Row className="row">
+        <Col className="row">
+          <div className="container-wrapper">
+            <div className="environment">Environment</div>
+            <div className="container-wrapper-auth">
+              <div className="tabs">
+                <div className="tabs-auth">
+                  <ul className="tab-group">
+                    <li className={`tab ${type === 'Sign In' ? 'active' : ''}`} onClick={() => setType('Sign In')}>
+                      <button className="login">Sign In</button>
+                    </li>
+                    <li className={`tab ${type === 'Sign Up' ? 'active' : ''}`} onClick={() => setType('Sign Up')}>
+                      <button className="signup">Sign Up</button>
+                    </li>
+                  </ul>
+                  {type === 'Sign In' && <div className="tab-item">LOGIN</div>}
+                  {type === 'Sign Up' && <div className="tab-item">REGISTER</div>}
+                </div>
+              </div>
+            </div>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default AuthTam;
