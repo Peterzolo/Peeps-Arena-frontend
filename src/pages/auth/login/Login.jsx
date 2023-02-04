00f85ae285fdcaebe7828135fdab717ea0476eb2
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-02-04 10:03:56 +0100

    Login
    Modified

--- src/pages/auth/login/Login.jsx
+++ src/pages/auth/login/Login.jsx
@@ -1,7 +1,8 @@
+import './Login.scss';
 export const Login = () => {
   return (
     <div className="wrapper">
-      <h3 className="login">Login</h3>
+      <h3 className="login-heading">Login</h3>
     </div>
   );
 };
