24e4ffefa5e5a0a42124fe146d5c109d0aaaf90f
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-02-04 09:27:02 +0100

    Update Card.jsx
    Added card component

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,22 +1,18 @@
-import React from 'react';
-import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
-export const Card = (props) => {
-  // eslint-disable-next-line react/prop-types
-  const { children, ...rest } = props;
-  return <Components {...rest}>{children}</Components>;
+const Button = (props) => {
+  const { className, children } = props;
+
+  return (
+    <>
+      <div className={className}>{children}</div>
+    </>
+  );
 };
 
-const Components = styled.div`
-  /* background-color: ${(props) => props.theme.backgroundColors.secondaryBg}; */
-  background-color: green;
-  width: max-content;
-  height: max-content;
-  color: ${(props) => props.theme.colors.dark};
-  padding: ${(props) => props.theme.padding.fullOne};
-  margin-top: 20px;
-  border-radius: ${(props) => props.theme.border.rounded};
-  /* margin-bottom: ${(props) => props.theme.margin.bottom}; */
-`;
+Button.propTypes = {
+  children: PropTypes.any,
+  className: PropTypes.string
+};
 
-export default Card;
+export default Button;
