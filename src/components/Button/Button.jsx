6d11b81c816fdac9d8b0b8ce8c3eb381a1afeb87
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-02-04 09:11:29 +0100

    Components
    Added button components

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,19 +1,22 @@
-import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
-export const Button = (props) => {
-  // eslint-disable-next-line react/prop-types
-  const { children, ...rest } = props;
-  return <Components {...rest}>{children}</Components>;
+const Button = (props) => {
+  const { label, className, disabled, handleClick } = props;
+
+  return (
+    <>
+      <button className={className} onClick={handleClick} disabled={disabled}>
+        {label}
+      </button>
+    </>
+  );
 };
 
-const Components = styled.button`
-  color: ${(props) => props.theme.colors.dark};
-  font-weight: ${(props) => props.theme.fontWeight.light2};
-  font-size: ${(props) => props.theme.fontSize.small};
-  width: max-content;
-  height: max-content;
-  border: 1px solid ${(props) => props.theme.backgroundColors.primaryBg};
-  background-color: transparent;
-  padding: 15px;
-  border-radius: 5px;
-`;
+Button.propTypes = {
+  label: PropTypes.any.isRequired,
+  className: PropTypes.string,
+  handleClick: PropTypes.func,
+  disabled: PropTypes.bool
+};
+
+export default Button;
