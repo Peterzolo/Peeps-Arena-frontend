8063f067238c795c90f1c6efbf07c4c470366876
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-02-05 17:25:25 +0100

    Auth tab
    Modified

--- src/pages/auth/auth-tab/AuthTab.jsx
+++ src/pages/auth/auth-tab/AuthTab.jsx
@@ -3,41 +3,34 @@ import { Col, Container, Row } from 'react-bootstrap';
 import { Card } from '../../../components/card/Card';
 
 import '../auth-tab/AuthTab.scss';
-import { Login } from '../login/Login';
-import { Register } from '../register/Register';
+// import { Login } from '../login/Login';
+// import { Register } from '../register/Register';
 
 const AuthTab = () => {
+  // eslint-disable-next-line no-unused-vars
   const [type, setType] = useState('Sign In');
   return (
     <Container className="main-wrapper">
-      <Row className="row">
-        <Col className="row" ms={12}>
-          <div className="tabs-auth">
-            <ul className="tab-group">
-              <li className={`tab ${type === 'Sign In' ? 'active' : ''}`} onClick={() => setType('Sign In')}>
-                <button className="login-btn">Sign In</button>
-              </li>
-              <li className={`tab ${type === 'Sign Up' ? 'active' : ''}`} onClick={() => setType('Sign Up')}>
-                <button className="register-btn">Sign Up</button>
-              </li>
-            </ul>
-            <div className="form-wrap">
+      <div className="sub-container">
+        <Row className="row">
+          <Col className="row" ms={12}>
+            <div className="tabs-auth">
+              <ul className="tab-group">
+                <li className={`tab ${type === 'Sign In' ? 'active' : ''}`} onClick={() => setType('Sign In')}>
+                  <button className="login-btn">Sign In</button>
+                </li>
+                <li className={`tab ${type === 'Sign Up' ? 'active' : ''}`} onClick={() => setType('Sign Up')}>
+                  <button className="register-btn">Sign Up</button>
+                </li>
+              </ul>
               <Card className="card-wrap">
-                {type === 'Sign In' && (
-                  <div className="tab-item">
-                    <Login />
-                  </div>
-                )}
-                {type === 'Sign Up' && (
-                  <div className="tab-item">
-                    <Register />
-                  </div>
-                )}
+                {type === 'Sign In' && <div className="tab-item">{/* <Login /> */}</div>}
+                {type === 'Sign Up' && <div className="tab-item">{/* <Register /> */}</div>}
               </Card>
             </div>
-          </div>
-        </Col>
-      </Row>
+          </Col>
+        </Row>
+      </div>
     </Container>
   );
 };
