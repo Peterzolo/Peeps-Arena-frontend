24d544f6fc0d30a2d584cf72ccdb06d372c774ed
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-02-05 19:24:19 +0100

    Update Login component
    Modification

--- src/pages/auth/login/Login.jsx
+++ src/pages/auth/login/Login.jsx
@@ -1,6 +1,14 @@
+import { useState } from 'react';
 import { Container } from 'react-bootstrap';
+import { Button } from '../../../components/Button/Button';
+import './Login.scss';
+
+const hasError = false;
+const errorMessage = 'Error message';
+const loading = false;
 
 export const Login = () => {
+  const [alertType] = useState('');
   return (
     <Container className="login-container">
       <div className="sub-container">
@@ -11,7 +19,13 @@ export const Login = () => {
         )}
 
         <div className="form-container">
-          <form action="" className="wrap"></form>
+          <form action="" className="form-wrap">
+            <Button
+              label={`${loading ? 'SIGNIN IN PROGRESS...' : 'SIGNIN'}`}
+              className="send-btn"
+              // disabled={!username || !password}
+            />
+          </form>
         </div>
       </div>
     </Container>
