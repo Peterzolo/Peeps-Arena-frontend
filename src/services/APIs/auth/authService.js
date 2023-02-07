40d083a7cbbc4734b0d50cd3ee4227203a032033
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-02-07 08:15:12 +0100

    Update authService.js
    Added login method

--- src/services/APIs/auth/authService.js
+++ src/services/APIs/auth/authService.js
@@ -5,6 +5,11 @@ class AuthService {
     const response = await axios.post('/register', body);
     return response;
   }
+
+  async signIn(body) {
+    const response = await axios.post('/login', body);
+    return response;
+  }
 }
 
 export const authService = new AuthService();
