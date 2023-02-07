e5760c88e43e734d4306de3314aab1966de1d4bd
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-02-07 01:13:26 +0100

    Forgot password
    Added styles

--- src/pages/auth/auth-tab/forgot-password/ForgotPassword.jsx
+++ src/pages/auth/auth-tab/forgot-password/ForgotPassword.jsx
@@ -7,46 +7,43 @@ import { Card } from '../../../../components/card/Card';
 
 import './ForgotPassword.scss';
 
-const hasError = true;
+const hasError = false;
 const errorMessage = 'Error message';
-const loading = false;
+const loading = true;
 
 export const ForgotPassword = () => {
   const [email, setEmail] = useState('');
   return (
     <Container className="forgot-password-container">
       <Card className="card-wrap">
-        <div className="sub-container">
-          {hasError && errorMessage && (
-            // <div className={`alerts ${alertType}`} role="alert">
-            //   {errorMessage}
-            // </div>
-            <div className="alerts">Here is the error message gggfgfgfgfgfgfgfgfgfgfg</div>
-          )}
+        <div className="title">Forgot Password</div>
+        {hasError && errorMessage && (
+          // <div className={`alerts ${alertType}`} role="alert">
+          //   {errorMessage}
+          // </div>
+          <div className="alerts">Here is the error message gggfgfgfgfgfgfgfgfgfgfg</div>
+        )}
+        <div className="form-container">
+          <form action="" className="form-wrap">
+            <Input
+              id="email"
+              name="email"
+              type="text"
+              value={email}
+              labelText="email"
+              placeholder="Enter Email"
+              handleChange={(event) => setEmail(event.target.value)}
+              className="input"
+            />
 
-          <div className="form-container">
-            <form action="" className="form-wrap">
-              <Input
-                id="email"
-                name="email"
-                type="email"
-                value={email}
-                labelText="Email"
-                placeholder="Enter Email"
-                handleChange={(event) => setEmail(event.target.value)}
-              />
-
-              <Button label={`${loading ? 'REQUEST IN PROGRESS...' : 'SEND'}`} className="send-btn" disabled={!email} />
-              <div className="forgot-password">Forgot Password?</div>
-              <hr />
-              <div className="remarks-wrap">
-                <div className="remark-text">Back to login</div>
-                <Link to={'/'} style={{ textDecoration: 'none', color: '#722ed1' }}>
-                  Login
-                </Link>
-              </div>
-            </form>
-          </div>
+            <Button label={`${loading ? 'REQUEST IN PROGRESS...' : 'SEND'}`} className="send-btn" disabled={!email} />
+          </form>
+        </div>
+        <div className="remarks-wrap">
+          <div className="remark-text">Back to</div>
+          <Link to={'/auth'} style={{ textDecoration: 'none', color: '#722ed1' }}>
+            Log In
+          </Link>
         </div>
       </Card>
     </Container>
