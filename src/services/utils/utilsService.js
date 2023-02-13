3110886d5069e5cd4500739ae0f76f25111f5f5d
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-02-13 21:21:17 +0100

    Update utilsService.js
    Added user dispatch

--- src/services/utils/utilsService.js
+++ src/services/utils/utilsService.js
@@ -1,5 +1,6 @@
 import { floor, random } from 'lodash';
 import { avatarColors } from '@services/utils/static.data';
+import { addUser } from '@redux/reducers/user/userReducer';
 
 export class Utils {
   static avatarColor() {
@@ -23,5 +24,11 @@ export class Utils {
     context.fillText(text, canvas.width / 2, canvas.height / 2);
 
     return canvas.toDataURL('image/png');
+  }
+
+  static dispatchUser(result, pageReload, dispatch, setUser) {
+    pageReload(true);
+    dispatch(addUser({ token: result.data.token, profile: result.data.user }));
+    setUser(result.data.user);
   }
 }
