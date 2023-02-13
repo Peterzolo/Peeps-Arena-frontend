3301e7a0648570ded1d735ee63f1057337241df2
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-02-13 17:45:03 +0100

    Update store.js
    Modified

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './reducers/user/userReducer';
 
 export const store = configureStore({
-  reducers: {
+  reducer: {
     user: userReducer
   }
 });
