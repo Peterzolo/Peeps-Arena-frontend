c8bb723226a665efa1c761a920ff69d4e32d38cb
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-02-14 17:20:31 +0100

    Header
    Added header component

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,7 @@
 import { useRoutes } from 'react-router-dom';
 import { ForgotPassword, Login, PasswordReset, Register } from '@pages/auth/index';
 import Streams from '@pages/social/streams/Streams';
-import Social from './pages/social/Social';
+import Social from '@pages/social/Social';
 
 export const AppRouter = () => {
   const elements = useRoutes([
@@ -24,7 +24,7 @@ export const AppRouter = () => {
     {
       path: '/app/social',
       element: <Social />,
-      children: [{ path: '/streams', element: <Streams /> }]
+      children: [{ path: 'streams', element: <Streams /> }]
     }
   ]);
 
