27ad020864625be1cd7ffcfe5e9e5ce3a286562c
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-02-14 16:51:07 +0100

    Socials
    Added div for header

---
+++ src/pages/social/Social.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import '@pages/social/Social.scss';
+
+const Social = () => {
+  return (
+    <>
+      <div className="header">Header</div>
+    </>
+  );
+};
+
+export default Social;
