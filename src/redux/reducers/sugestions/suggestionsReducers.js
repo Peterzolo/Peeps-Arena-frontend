166e765c76c6caec4cdad9a767d246f20bebfc71
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-02-15 22:12:25 +0100

    Update suggestionsReducers.js
    Added suggestion reducers

--- src/redux/reducers/sugestions/suggestionsReducers.js
+++ src/redux/reducers/sugestions/suggestionsReducers.js
@@ -4,3 +4,18 @@ const initialState = {
   users: [],
   isLoading: false
 };
+
+const suggestionsSlice = createSlice({
+  name: 'suggestions',
+  initialState,
+  reducers: {
+    addToSuggestions: (state, action) => {
+      const { users, isLoading } = action.payload;
+      state.users = [...users];
+      state.isLoading = isLoading;
+    }
+  }
+});
+
+export const { addToSuggestions } = suggestionsSlice.actions;
+export default suggestionsSlice.reducer;
