561b4eddcf6e896fed64f9dde35bfcb3353c6fd9
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-02-15 07:50:30 +0100

    Update Suggestions.jsx
    Adde html contents

--- src/components/suggestions/Suggestions.jsx
+++ src/components/suggestions/Suggestions.jsx
@@ -1,8 +1,50 @@
 import React from 'react';
 import '@components/suggestions/Suggestiions.scss';
+import { Avatar } from '../avatar/Avatar';
+import { Button } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+
+const users = [];
 
 const Suggestions = () => {
-  return <div></div>;
+  const navigate = useNavigate();
+  return (
+    <div className="suggestions-list-container" data-testid="suggestions-container">
+      <div className="suggestions-header">
+        <div className="title-text">Suggestions</div>
+      </div>
+      <hr />
+      <div className="suggestions-container">
+        <div className="suggestions">
+          {users?.map((user) => (
+            <div data-testid="suggestions-item" className="suggestions-item" key={user?._id}>
+              <Avatar
+                name={user?.username}
+                bgColor={user?.avatarColor}
+                textColor="#ffffff"
+                size={40}
+                avatarSrc={user?.profilePicture}
+              />
+              <div className="title-text">{user?.username}</div>
+              <div className="add-icon">
+                <Button
+                  label="Follow"
+                  className="button follow"
+                  disabled={false}
+                  // handleClick={() => followUser(user)}
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+        {users.length > 8 && (
+          <div className="view-more" onClick={() => navigate('/app/social/people')}>
+            View More
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Suggestions;
