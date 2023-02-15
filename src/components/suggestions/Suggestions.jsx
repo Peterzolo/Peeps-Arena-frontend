55eb2c2ccfc772f4594579f6f694ebc2b7de1a5f
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-02-15 07:53:14 +0100

    Update Suggestions.jsx
    Modified import

--- src/components/suggestions/Suggestions.jsx
+++ src/components/suggestions/Suggestions.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '@components/suggestions/Suggestiions.scss';
-import { Avatar } from '../avatar/Avatar';
-import { Button } from 'react-bootstrap';
+import { Avatar } from '@components/avatar/Avatar';
+import { Button } from '@components/button/Button';
 import { useNavigate } from 'react-router-dom';
 
 const users = [];
