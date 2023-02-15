e143f249f7798a79550574a686149d43f9a767e4
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-02-15 07:17:04 +0100

    Update Streams.jsx
    Html contents

--- src/pages/social/streams/Streams.jsx
+++ src/pages/social/streams/Streams.jsx
@@ -4,7 +4,12 @@ import '@pages/social/streams/Streams.scss';
 const Streams = () => {
   return (
     <div className="streams">
-      <div className="streams-content"></div>
+      <div className="streams-content">
+        <div className="streams-post">
+          <div className="post-form">Post Form</div>
+          <div className="post-items">Posts Items</div>
+        </div>
+      </div>
     </div>
   );
 };
