a987f7d2500df98faf9e982b3f28bb8a31ab3a31
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-02-15 05:41:02 +0100

    Update Sidebar.jsx
    Add html element

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
-import { sideBarItems, fontAwesomeIcons } from 'src/services/utils/static.data';
+import React, { useEffect, useState } from 'react';
+import { sideBarItems, fontAwesomeIcons } from '@services/utils/static.data';
 import '@component/sidebar.Sidebar.scss';
 
 const Sidebar = () => {
-  return <div></div>;
+  const [sidebar, setSidebar] = useState([]);
+
+  useEffect(() => {
+    setSidebar(sideBarItems);
+  }, []);
+
+  return (
+    <div className="app-side-menu">
+      <div className="side-menu"></div>
+    </div>
+  );
 };
 
 export default Sidebar;
