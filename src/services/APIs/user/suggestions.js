86a983d3ab349389a75404fae794a72ee2dba75d
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-02-15 22:26:32 +0100

    Create suggestions.js
    Added user service

---
+++ src/services/APIs/user/suggestions.js
@@ -0,0 +1,10 @@
+import axios from '@services/axios';
+
+class UserService {
+  async getUserSuggestions() {
+    const response = await axios.get('/user/profile/user/suggestions');
+    return response;
+  }
+}
+
+export const userSerevice = new UserService();
