cb91abd454c0b7e704f692ae2cf354ca6fce090e
Peter <petersolomon704@gmail.com>
2023-02-24 07:44:43 +0100

    Chart
    Added chart pages

---
+++ src/pages/chart/Chart.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '@pages/chart/Chart.scss';
+
+const Chart = () => {
+  return <div>Chart</div>;
+};
+
+export default Chart;
---
+++ src/pages/people/People.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '@pages/people/People.scss';
+
+const People = () => {
+  return <div>People</div>;
+};
+
+export default People;
