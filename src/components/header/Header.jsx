d9d8d261950411d01f9322b7710f9e602e1f5f73
Peter <petersolomon704@gmail.com>
2023-03-08 21:34:37 +0100

    Notification preview
    Modification

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -296,7 +296,7 @@ const Header = () => {
                         notificationCount={0}
                         title="Settings"
                         onLogout={onLogout}
-                        onNavigate={() => ProfileUtils.navigateToProfile(profile, navigate)}
+                        // onNavigate={() => ProfileUtils.navigateToProfile(profile, navigate)}
                       />
                     </li>
                   </ul>
